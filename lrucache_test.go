c15d058880d0d1e197b9dc10a334486accb36a9c
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-05 21:20:58 +0900

    [test] Add a test for the capacity of LRUCache

--- lrucache_test.go
+++ lrucache_test.go
@@ -29,6 +29,46 @@ func TestLRUCache_Put(t *testing.T) {
 	}
 }
 
+func Test_LRUCache_Put_RemoveLastElementIfLenExceedCapacity(t *testing.T) {
+	lru := New(5)
+	lru.Put("a", "A") // a
+	lru.Put("b", "B") // b a
+	lru.Put("c", "C") // c b a
+	lru.Put("d", "D") // d c b a
+	lru.Put("e", "E") // e d c b a
+	if lru.l.Len() != 5 {
+		t.Errorf("Expected 5 but got %d", lru.l.Len())
+	}
+
+	lru.Put("f", "F") // expected to be like this: f e d c b
+
+	if lru.l.Len() != 5 {
+		t.Errorf("Expected 5 but got %d", lru.l.Len())
+	}
+
+	expectedFirst := []string{"f", "e", "d", "c", "b"}
+	expectedSecond := []string{"F", "E", "D", "C", "B"}
+
+	e := lru.l.Front()
+	for i := range 5 {
+		if e == nil {
+			t.Errorf("Expected something but got nil\n")
+		}
+
+		first := expectedFirst[i]
+		second := expectedSecond[i]
+		if e.Value.(Pair).First != first {
+			t.Errorf("Expected `%s` but got %s\n", first, e.Value.(Pair).First)
+		}
+		if e.Value.(Pair).Second != second {
+			t.Errorf("Expected `%s` but got %s\n", second, e.Value.(Pair).Second)
+		}
+
+		e = e.Next()
+	}
+	
+}
+
 func Test_LRUCache_Get(t *testing.T) {
 	lru := New(10)
 	lru.Put("a", "A")
