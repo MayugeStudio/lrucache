2800e2a232e85a48dbba67bf891e745910cd9462
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-05 21:19:28 +0900

    [fix] Fix tests

--- lrucache_test.go
+++ lrucache_test.go
@@ -7,40 +7,40 @@ import (
 )
 
 func TestLRUCache_Put(t *testing.T) {
-	lru := New()
-	lru.Put("<abc>", "</abc>")
-	lru.Put("<def>", "</def>")
-	lru.Put("<ghi>", "</ghi>")
+	lru := New(10)
+	lru.Put("a", "A")
+	lru.Put("b", "B")
+	lru.Put("c", "C")
 
 	if lru.l.Len() != 3 {
 		t.Errorf("Expected 3 but got %d", lru.l.Len())
 	}
 
-	if lru.l.Front().Value.(Pair).First != "<abc>" {
-		t.Errorf("Expected 10 but got %d", lru.l.Front().Value)
+	if lru.l.Front().Value.(Pair).First != "c" {
+		t.Errorf("Expected `c` but got %s", lru.l.Front().Value.(Pair).First)
 	}
 
-	if lru.l.Front().Next().Value.(Pair).First != "<def>" {
-		t.Errorf("Expected 20 but got %d", lru.l.Front().Next().Value)
+	if lru.l.Front().Next().Value.(Pair).First != "b" {
+		t.Errorf("Expected `b` but got %s", lru.l.Front().Next().Value.(Pair).First)
 	}
 
-	if lru.l.Front().Next().Next().Value.(Pair).First != "<ghi>" {
-		t.Errorf("Expected 30 but got %d", lru.l.Front().Next().Next().Value)
+	if lru.l.Front().Next().Next().Value.(Pair).First != "a" {
+		t.Errorf("Expected `a` but got %s", lru.l.Front().Next().Next().Value.(Pair).First)
 	}
 }
 
 func Test_LRUCache_Get(t *testing.T) {
-	lru := New()
-	lru.Put("<abc>", "</abc>")
-	lru.Put("<def>", "</def>")
-	lru.Put("<ghi>", "</ghi>")
+	lru := New(10)
+	lru.Put("a", "A")
+	lru.Put("b", "B")
+	lru.Put("c", "C")
 
 	if lru.l.Len() != 3 {
 		t.Errorf("Expected 3 but got %d", 3)
 	}
 	
-	if r, ok := lru.Get("<abc>"); ok && r != "</abc>" {
-		t.Errorf("expected </abc> but got %s", r)
+	if r, ok := lru.Get("a"); ok && r != "A" {
+		t.Errorf("expected A but got %s", r)
 	}
 }
 
