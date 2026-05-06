decb9c4955ede473263d7bdcc62b1e1800379dc4
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-06 09:38:41 +0900

    [test] Update tests

--- lrucache_test.go
+++ lrucache_test.go
@@ -14,26 +14,46 @@ func TestLRUCache_Put(t *testing.T) {
 		t.Fatalf("Expected 3 but got %d\n", lru.l.Len())
 	}
 
-	if lru.l.Front().Value.(Pair).First != "c" {
-		t.Fatalf("Expected `c` but got %s\n", lru.l.Front().Value.(Pair).First)
+	if lru.l.Front().Value.(string) != "c" {
+		t.Fatalf("Expected `c` but got %s\n", lru.l.Front().Value.(string))
 	}
 
-	if lru.l.Front().Next().Value.(Pair).First != "b" {
-		t.Fatalf("Expected `b` but got %s\n", lru.l.Front().Next().Value.(Pair).First)
+	if value, ok := lru.cache["c"]; !ok {
+		t.Fatalf("Expected `c` to be in the cache but it is not\n")
+	} else if value != "C" {
+		t.Fatalf("Expected `C` but got %s\n", value)
 	}
 
-	if lru.l.Front().Next().Next().Value.(Pair).First != "a" {
-		t.Fatalf("Expected `a` but got %s\n", lru.l.Front().Next().Next().Value.(Pair).First)
+
+	if lru.l.Front().Next().Value.(string) != "b" {
+		t.Fatalf("Expected `b` but got %s\n", lru.l.Front().Next().Value.(string))
+	}
+
+	if value, ok := lru.cache["b"]; !ok {
+		t.Fatalf("Expected `b` to be in the cache but it is not\n")
+	} else if value != "B" {
+		t.Fatalf("Expected `B` but got %s\n", value)
+	}
+
+
+	if lru.l.Front().Next().Next().Value.(string) != "a" {
+		t.Fatalf("Expected `a` but got %s\n", lru.l.Front().Next().Next().Value.(string))
+	}
+
+	if value, ok := lru.cache["a"]; !ok {
+		t.Fatalf("Expected `a` to be in the cache but it is not\n")
+	} else if value != "A" {
+		t.Fatalf("Expected `A` but got %s\n", value)
 	}
 }
 
 func Test_LRUCache_Put_RemoveLastElementIfLenExceedCapacity(t *testing.T) {
 	lru := New(5)
-	lru.Put("a", "A") // a
-	lru.Put("b", "B") // b a
-	lru.Put("c", "C") // c b a
-	lru.Put("d", "D") // d c b a
-	lru.Put("e", "E") // e d c b a
+	lru.Put("a", "A") // l: a
+	lru.Put("b", "B") // l: b a
+	lru.Put("c", "C") // l: c b a
+	lru.Put("d", "D") // l: d c b a
+	lru.Put("e", "E") // l: e d c b a
 	if lru.l.Len() != 5 {
 		t.Fatalf("Expected 5 but got %d\n", lru.l.Len())
 	}
@@ -44,8 +64,8 @@ func Test_LRUCache_Put_RemoveLastElementIfLenExceedCapacity(t *testing.T) {
 		t.Fatalf("Expected 5 but got %d\n", lru.l.Len())
 	}
 
-	expectedFirst := []string{"f", "e", "d", "c", "b"}
-	expectedSecond := []string{"F", "E", "D", "C", "B"}
+	keys := []string{"f", "e", "d", "c", "b"}
+	values := []string{"F", "E", "D", "C", "B"}
 
 	e := lru.l.Front()
 	for i := range 5 {
@@ -53,18 +73,16 @@ func Test_LRUCache_Put_RemoveLastElementIfLenExceedCapacity(t *testing.T) {
 			t.Fatalf("Expected something but got nil\n")
 		}
 
-		first := expectedFirst[i]
-		second := expectedSecond[i]
-		if e.Value.(Pair).First != first {
-			t.Fatalf("Expected `%s` but got %s\n", first, e.Value.(Pair).First)
-		}
-		if e.Value.(Pair).Second != second {
-			t.Fatalf("Expected `%s` but got %s\n", second, e.Value.(Pair).Second)
+		key := keys[i]
+		expectedValue := values[i]
+		if value, ok := lru.cache[key]; !ok {
+			t.Fatalf("Expected `%s` to be in the cache but it is not", key)
+		} else if value != expectedValue {
+			t.Fatalf("Expected `%s` but got %s\n", value, expectedValue)
 		}
 
 		e = e.Next()
 	}
-
 }
 
 func Test_LRUCache_Get(t *testing.T) {
