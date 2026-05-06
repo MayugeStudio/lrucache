4b6b2829f22337f4282367fe3f6e84d33dc59702
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-06 14:23:24 +0900

    Add benchmarks

--- lrucache_test.go
+++ lrucache_test.go
@@ -2,7 +2,10 @@ package lrucache
 
 import (
 	"testing"
+	"fmt"
+	"strings"
 )
+
 
 func TestLRUCache_Put(t *testing.T) {
 	lru := New(10)
@@ -135,5 +138,56 @@ func Test_LRUCache_Cap1(t *testing.T) {
 	} else if value != "B" {
 		t.Fatalf("Expected `B` but got %s\n", value)
 	}
+}
+
+
+func Benchmark_Put(b *testing.B) {
+	lru := New(20)
+
+	b.ResetTimer()
+
+	for i := 0; i<b.N; i++ {
+		lru.Put(fmt.Sprintf("Lower: %d", i), strings.ToUpper(fmt.Sprintf("Upper: %d", i)))
+	}
+}
+
+func Benchmark_Get(b *testing.B) {
+	lru := New(1000)
+	for i := 0; i<1000; i++ {
+		lru.Put(fmt.Sprintf("Lower: %d", i), strings.ToUpper(fmt.Sprintf("Upper: %d", i)))
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i<b.N; i++ {
+		key := i % 1000
+		lru.Get(fmt.Sprintf("Lower: %d", key))
+	}
+}
+
+// This testing method is based on HashiCorp golang-lru benchmark 
+func Benchmark_Random(b *testing.B) {
+	lru := New(4096)
+	trace := make([]string, b.N*2)
+	for i := 0; i<b.N*2; i++ {
+		trace[i] = fmt.Sprintf("%d", getRand(b) % 16384)
+	}
+
+	b.ResetTimer()
+
+	var hit, miss int
+	for i := 0; i<b.N*2; i++ {
+		if i % 2 == 0 {
+			lru.Put(trace[i], trace[i])
+		} else {
+			if _, ok := lru.Get(trace[i]); ok {
+				hit += 1
+			} else {
+				miss += 1
+			}
+		}
+	}
+
+	b.Logf("hit: %d, miss: %d, ratio: %f", hit, miss, float64(hit)/float64(miss+hit))
 }
 
---
+++ testing_test.go
@@ -0,0 +1,20 @@
+// Copyright IBM Corp. 2014, 2025
+// SPDX-License-Identifier: MPL-2.0
+// Modified: packege name changed
+
+package lrucache
+
+import (
+	"crypto/rand"
+	"math"
+	"math/big"
+	"testing"
+)
+
+func getRand(tb testing.TB) int64 {
+	out, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
+	if err != nil {
+		tb.Fatal(err)
+	}
+	return out.Int64()
+}
